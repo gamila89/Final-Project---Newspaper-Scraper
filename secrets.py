4cad9e01dc0687c7453578eba0b309c4ecae3f4e
gamila89 <80641259+gamila89@users.noreply.github.com>
2021-04-28 10:31:51 -0400

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,9 @@
+#fill this in with your credentials and rename to secrets.py
+#make sure to gitignore this file! you will lose points if you share this in your github!
+
+
+TWITTER_API_KEY="TiuI0xmtqIctoZJNNeHLjebkX"
+TWITTER_API_SECRET="3TOPUVqEMnBPI379MEnGO4FQ8zxkiR3lc8vom6hhWFiBV5aXGp"
+TWITTER_ACCESS_TOKEN="1369872642933678083-TIDgjPFsIODqfjcjELh3wJWVlQtbOc"
+TWITTER_ACCESS_TOKEN_SECRET="GfoK1N0UffRzrfMGAMEbheuIDDsrtGuGAY9rkDTHF3F1m"
+
